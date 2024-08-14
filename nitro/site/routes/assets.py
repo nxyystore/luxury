156201f2a8a22afb64b07f6ba73f9ea61cf9dcdd
Nxyy <nxyy@nxyy.store>
2024-08-14 08:46:52 -0400

    Delete nitro directory

--- nitro/site/routes/assets.py
+++
@@ -1,6 +0,0 @@
-from main import app
-from flask import send_from_directory
-
-@app.route('/assets/<path:path>')
-def assets(path):
-    return send_from_directory('assets', path)
--- nitro/sniper/auth_server/utils/b64.py
+++
@@ -1,7 +0,0 @@
-import base64
-
-f = open('server.pub', 'rb')
-pub_key = f.read()
-f.close()
-
-print(base64.b64encode(pub_key).decode())
